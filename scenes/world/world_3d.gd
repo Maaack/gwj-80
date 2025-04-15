e9405fdb7a998176c25aa33951eb6f7f235779ea
Marek Belski <marek.belski@gmail.com>
2025-04-14 18:18:16 -0700

    Tests slime delivery in world 3d.

--- scenes/world/world_3d.gd
+++ scenes/world/world_3d.gd
@@ -3,6 +3,7 @@ extends Node3D
 signal level_won
 
 @export var objective_list : ObjectiveList
+@export var delivery_area : DeliveryArea3D
 
 var level_state : LevelState
 var slimes_submitted : Dictionary[Constants.SlimeType, int] = {}
@@ -20,14 +21,23 @@ func _check_level_won() -> void:
 	if _check_objectives_completed():
 		level_won.emit()
 
-func _on_slime_submitted(slime_data : SlimeData):
+func _on_slime_delivered(slime_data : SlimeData):
 	if not slimes_submitted.has(slime_data.slime_type):
 		slimes_submitted[slime_data.slime_type] = 0
 	slimes_submitted[slime_data.slime_type] += 1
 	_check_level_won()
 
+func _on_slime_detected():
+	$DeliveryDelayTimer.start()
+
+func _on_delivery_delay_timer_timeout():
+	delivery_area.deliver()
+
 func _ready():
 	level_state = GameState.get_level_state(scene_file_path)
+	if delivery_area:
+		delivery_area.slime_delivered.connect(_on_slime_delivered)
+		delivery_area.slime_detected.connect(_on_slime_detected)
 
 func _exit_tree():
 	GlobalState.save()
