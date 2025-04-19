c60f3a90fb08d0b01e58dd7cfbd5472a927db4e6
Marek Belski <marek.belski@gmail.com>
2025-04-18 21:59:09 -0700

    Animates journal opening and closing.

--- scenes/game_scene/journal_panel.gd
+++ scenes/game_scene/journal_panel.gd
@@ -1,5 +1,25 @@
+@tool
+class_name JournalPanel
 extends PanelContainer
+
+@export var open : bool = false :
+	set(value):
+		open = value
+		if is_inside_tree():
+			_update_state()
+
+
+@onready var _animation_player : AnimationPlayer = $AnimationPlayer
+
+func _update_state() -> void:
+	if _animation_player.is_playing(): return
+	if open:
+		_animation_player.play(&"OPEN")
+	else:
+		_animation_player.play(&"CLOSE")
+	
 
 func _input(event) -> void:
 	if event.is_action_pressed(&"journal"):
-		visible = !visible
+		if _animation_player.is_playing(): return
+		open = !open
