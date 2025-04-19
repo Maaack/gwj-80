77a9007cdcf23153d7377f123bb53c2c811bb071
Marek Belski <marek.belski@gmail.com>
2025-04-18 22:23:11 -0700

    Captures mouse when opening journal.

--- scenes/game_scene/journal_panel.gd
+++ scenes/game_scene/journal_panel.gd
@@ -2,6 +2,9 @@
 class_name JournalPanel
 extends PanelContainer
 
+signal journal_opened
+signal journal_closed
+
 @export var open : bool = false :
 	set(value):
 		open = value
@@ -15,9 +18,12 @@ func _update_state() -> void:
 	if _animation_player.is_playing(): return
 	if open:
 		_animation_player.play(&"OPEN")
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		journal_opened.emit()
 	else:
 		_animation_player.play(&"CLOSE")
-	
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		journal_closed.emit()
 
 func _input(event) -> void:
 	if event.is_action_pressed(&"journal"):
