4fcbd07eda4a642eb18ebc1c603af6e8358b7572
Marek Belski <marek.belski@gmail.com>
2025-04-19 01:20:14 -0700

    Faster delivery of multiple slimes.

--- scenes/world/delivery_area_3d.gd
+++ scenes/world/delivery_area_3d.gd
@@ -4,17 +4,22 @@ extends Area3D
 signal slime_delivered(slime_data : SlimeData)
 signal slime_detected
 
+@export var cooldown_time : float = 0.1
 @export var delivery_time : float = 1.0
 
 var slimes_detected : Array[Slime]
 
-func deliver():
+func deliver_slime(slime : Slime) -> void:
+	var slime_data = slime.slime_data
+	slime.depart(delivery_time)
+	await slime.departed
+	slime_delivered.emit(slime_data)
+
+func deliver() -> void:
 	if slimes_detected.size() == 0: return
 	for slime in slimes_detected:
-		var slime_data = slime.slime_data
-		slime.depart(delivery_time)
-		await slime.departed
-		slime_delivered.emit(slime_data)
+		deliver_slime(slime)
+		await get_tree().create_timer(cooldown_time, false).timeout
 
 func _on_body_entered(body : Node3D):
 	if body is Slime:
