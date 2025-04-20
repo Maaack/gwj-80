6db8a8c0d8b34b34a4db2a6e792e260a5b8c554b
Mjb <mjb117@terpmail.umd.edu>
2025-04-19 21:56:38 -0400

    Add slime combinations to journal

--- resources/data/slime_combination.gd
+++ resources/data/slime_combination.gd
@@ -4,4 +4,7 @@ extends Resource
 @export var slime_1 : Constants.SlimeType
 @export var slime_2 : Constants.SlimeType
 @export var slime_result : Constants.SlimeType
-	
+
+
+func has_slime(slime_type: Constants.SlimeType) -> bool:
+	return slime_type == slime_1 or slime_type == slime_2 or slime_type == slime_result
