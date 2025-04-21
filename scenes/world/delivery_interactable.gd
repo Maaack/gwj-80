468aefc57323db9331bfc6331c206082ca1634c9
Matt <mjb117@terpmail.umd.edu>
2025-04-20 23:35:13 -0400

    Delivery interactable updates (#65)
    * Display the delivery interactables' cooldowns
    
    * Add delivery box timer and cooldown icon

--- scenes/world/delivery_interactable.gd
+++ scenes/world/delivery_interactable.gd
@@ -5,6 +5,16 @@ signal interacted
 
 var is_cooldown : bool = false
 
+@onready var timer_icon: Sprite3D = %TimerIcon
+@onready var cooldown_timer: Timer = %CooldownTimer
+@onready var cooldown_label: Label3D = %CooldownLabel
+
+
+func _process(delta: float) -> void:
+	if cooldown_label.visible:
+		cooldown_label.text = "%0.1f" % cooldown_timer.time_left
+
+
 func _on_area_3d_body_entered(body):
 	if body is PlayerCharacter:
 		body.interactable = self
@@ -19,8 +29,12 @@ func interact():
 	$Area3D/CollisionShape3D.disabled = true
 	interacted.emit()
 	$CooldownTimer.start()
+	cooldown_label.show()
+	timer_icon.show()
 	is_cooldown = true
 
 func _on_cooldown_timer_timeout():
 	is_cooldown = false
+	cooldown_label.hide()
+	timer_icon.hide()
 	$Area3D/CollisionShape3D.disabled = false
